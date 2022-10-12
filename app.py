ad9845734b4f809b38833eb51962566ce944ee42
doviettu6720000 <doviettu672000@gmail.com>
2022-10-12 10:45:30 +0700

    fix-methods

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, jsonify
-
 from chat import get_response
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/",methods = ['GET'])
 def index_get():
     return render_template("base.html")
 
-@app.post("/predict")
+@app.route("/predict",methods = ['POST'])
 def predict():
     text = request.get_json().get("message")
     response = get_response(text)
